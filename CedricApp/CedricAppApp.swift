1a72a184668167a4205daa251683f3e5e0a18fee
bbloch <bbloch@veepee.com>
2025-01-22 17:00:44 +0100

    Initial Commit

---
+++ CedricApp/CedricAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CedricAppApp.swift
+//  CedricApp
+//
+//  Created by Bertrand Bloc'h on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct CedricAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CedricAppTests/CedricAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CedricAppTests.swift
+//  CedricAppTests
+//
+//  Created by Bertrand Bloc'h on 22/01/2025.
+//
+
+import Testing
+@testable import CedricApp
+
+struct CedricAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
